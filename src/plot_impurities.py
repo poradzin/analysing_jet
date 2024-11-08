15724934d773555b5ea2f4fdb5ddb7ac3adedfaa
mporad <michal.poradzinski@ukaea.uk>
2024-11-08 10:39:01 +0000

    Add imp Zeff

--- src/plot_impurities.py
+++ src/plot_impurities.py
@@ -21,21 +21,25 @@ def sum_nimp_with_charges(profs):
     total_density = np.zeros_like(profs.transp('NE'))
     imp_density = {}
     imp_el_density = {}
+    imp_zeff = {}
     for impurity in profs.impurities:   
         imp_el_density[impurity] = np.zeros_like(profs.transp('NE'))
         imp_density[impurity] = np.zeros_like(profs.transp('NE'))
+        imp_zeff[impurity] = np.zeros_like(profs.transp('NE'))
         for charge_state in profs.imp_charges[impurity]:  # Loop through charge states
             signal_name = f'NIMP_{impurity}_{charge_state}'  # Example: NIMP_NI_1, NIMP_NI_2, ...
             print(f'NIMP_{impurity}_{charge_state}')
             density = profs.transp(signal_name)  # Get the density for that charge state
             imp_el_density[impurity] += charge_state * density
+            imp_zeff[impurity] += density*charge_state**2
             imp_density[impurity] += density
             total_density += charge_state * density  # Multiply by charge state to get electron count
+        imp_zeff[impurity] = imp_zeff[impurity]/profs.transp('NE')
     if np.all(total_density == 0): 
         total_density = profs.xzimp*profs.transp('NIMP')
         imp_el_density = total_density
         imp_density = profs.transp('NIMP')
-    return (total_density, imp_el_density, imp_density)
+    return (total_density, imp_el_density, imp_density,imp_zeff)
 
 def sum_nimps_with_zimps(profs):
     total_density = np.zeros_like(profs.transp('NE'))
@@ -49,8 +53,8 @@ def sum_nimps_with_zimps(profs):
 imp_charge = {
     'BE': 4,  # Beryllium has a maximum charge state of 4
     'NI': 28, # and so on..
-    'NE': 10
-#    'W' : 74
+    'NE': 10,
+    'W' : 74
 }
 
 # NADVSIM: The NADVSIM(I) namelist variable controls this for 
@@ -68,9 +72,19 @@ time = float(sys.argv[3])
 
 profs = ps.Transp(pulse, runid)
 tind = ps.gi(profs.transp_time,time)
-signals = ('NE','NI','NMINI','BDENS')
+
+signals_start = ['NE','NI','NMINI','BDENS']
+signals = []
 #ion_signals = ['NI','NMINI','BDENS']
-ion_signals = ['NI','BDENS','NMINI']
+for sig in signals_start: 
+    if profs.check_signal(sig):
+        signals.append(sig)
+
+ion_signals_start = ['NI','BDENS','NMINI']
+ion_signals = []
+for sig in ion_signals_start:    
+    if profs.check_signal(sig):
+        ion_signals.append(sig)
 print(f'Impurities: {profs.impurities}')
 
 profs.add_data(*signals) 
@@ -81,7 +95,8 @@ profs.read_imp_charge_densities()
 #profs._imp_charges = {'BE':[4], 'NI':[28],'NE':[10]}
     
 #total_el_contribution, imp_el_density = sum_nimps_with_zimps(profs)
-total_imp_el_density, imps_el_densities, _ = sum_nimp_with_charges(profs)
+total_imp_el_density, imps_el_densities, _ , imp_zeff= sum_nimp_with_charges(profs)
+
 
 print(f'np.all(total_imp_el_density == 0): {np.all(total_imp_el_density == 0)}')
 if profs.xzimps is None:
@@ -96,7 +111,7 @@ nsum = np.sum([profs.transp(sig) for sig in ion_signals], axis=0) + total_imp_el
 win=pw.plotWindow()                                                                                 
                                                                                                         
 fig = plt.figure()                                                                                  
-fig.suptitle(f'Densities at {profs.t[tind]+40:.3f}s', fontsize=13)                                                      
+fig.suptitle(f'{profs.runcid} densities at {profs.t[tind]+40:.3f}s', fontsize=13)                                                      
 ax = fig.add_subplot(111)   
 ax.plot(
         profs.x[ps.gi(profs.transp_time,time)],
@@ -146,7 +161,7 @@ win.addPlot('Density profiles',fig)
 
 if profs.xzimps is not None:
     fig = plt.figure()                                                                                  
-    fig.suptitle(f'Imp. avg. effective charge at {profs.t[tind]+40:.3f}s', fontsize=13)                                                      
+    fig.suptitle(f'{profs.runcid} imp. avg. effective charge at {profs.t[tind]+40:.3f}s', fontsize=13)                                                      
     ax = fig.add_subplot(111)  
     for imp in profs.impurities: 
         ax.plot(
@@ -162,7 +177,7 @@ if profs.xzimps is not None:
    
 if profs.zeffp is not None: 
     fig = plt.figure()                                                                                  
-    fig.suptitle(f' 2D Effective charge at {profs.t[tind]+40:.3f}s', fontsize=13)                                                      
+    fig.suptitle(f'{profs.runcid} 2D Effective charge at {profs.t[tind]+40:.3f}s', fontsize=13)                                                      
     ax = fig.add_subplot(111)  
     ax.plot(
         profs.x[ps.gi(profs.transp_time,time)],
@@ -178,6 +193,13 @@ if profs.zeffp is not None:
             label='ZEFFPRO', 
             color='k',
             linestyle='--'
+            )
+    for imp in profs.impurities:                                                                    
+        ax.plot(                                                                                    
+            profs.x[ps.gi(profs.transp_time,time)],                                                 
+            imp_zeff[f'{imp}'][ps.gi(profs.transp_time,time)],                            
+            label=f'ZEFF_{imp}',                                                                   
+            linestyle='-',                                                                          
             )
     ax.set_xlabel(r'$\rho_{tor}^{norm}$')                                                                           
     leg=ax.legend()                                                                                     
